684d92ed634f02694a554c5182c52ccee57a487c
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2025-09-03 18:25:35 +0100

    Explorer page started

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Card } from "@/components/Cards";
+import NoResults from "@/components/NoResults";
+import Search from "@/components/Search";
+import { getProperties } from "@/lib/appwrite";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, FlatList, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Explore = () => {
-  return (
-    <View>
-      <Text>Explore</Text>
-    </View>
-  )
-}
+  const params = useLocalSearchParams<{
+    filters?: string;
+    query?: string;
+  }>();
 
-export default Explore
+  const {
+    refetch,
+    data: properties,
+    loading,
+  } = useAppwrite({
+    fn: getProperties,
+    params: { ...params, limit: 6 },
+    skip: true,
+  });
+  const handleCardPress = (id: string) => {
+    router.push(`/properties/${id}`);
+  };
+
+  useEffect(() => {
+    refetch({ filters: params.filters, query: params.query, limit: 8 });
+    //eslint-disable-next-line
+  }, [params.filters, params.query]);
+
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={properties}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32"
+        numColumns={2}
+        columnWrapperClassName="flex gap-5 px-5"
+        showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? <ActivityIndicator size={"large"} /> : <NoResults />
+        }
+        ListHeaderComponent={
+          <View className="px-5">
+            <Search />
+          </View>
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Explore;
